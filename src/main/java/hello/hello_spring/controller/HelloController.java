4932abd4b3b3d64454f5f134340e1cedd2bd3ed4
rladntlr <tlrrns08@gmail.com>
2025-04-29 17:19:19 +0900

    static content, MVC and templates engine, API

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,8 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,36 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Hello!!");
         return "hello"; //templates의 hello로 가라
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) { //name에 파라미터를 넘겨야함
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 body부분에 내가 데이터를 직접 넣어 주겠다
+    public String helloString(@RequestParam("name") String name) {
+        return "Hello " + name; // 화면이 아님 문자 그대로 내려줌
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello(); // 객체가 오면 json형식으로
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name; // key = name
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
