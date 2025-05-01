15309389a522a7fab0204310cba912fc4d7e8eb4
rladntlr <tlrrns08@gmail.com>
2025-05-02 04:01:05 +0900

    회원 서비스 테스트

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -9,7 +9,12 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    //DI(의존성 주입) 느슨한 결합으로 테스트와 확장성이 좋아짐
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     //회원 가입
     public Long join(Member member) {
